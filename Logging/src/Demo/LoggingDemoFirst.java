fcdacf5b1dc9ee19735561d080e511b32a76f5ca
Michajira <121868443+Michajira@users.noreply.github.com>
2023-01-04 16:41:31 +0530

    Update LoggingDemoFirst.java

--- Logging/src/Demo/LoggingDemoFirst.java
+++ Logging/src/Demo/LoggingDemoFirst.java
@@ -5,17 +5,17 @@ import org.apache.logging.log4j.*;
 public class LoggingDemoFirst {
 	
 	
-	private static Logger demologger = LogManager.getLogger(LoggingDemoFirst.class.getName());
+	private static Logger logger = LogManager.getLogger(LoggingDemoFirst.class.getName());
 
 	public static void main(String[] args)
 	{
 		
-		demologger.trace("This is trace");
-		demologger.debug("This is debug");
-		demologger.info("This is info");
-		demologger.warn("This is warn");
-		demologger.error("This is error");
-		demologger.fatal("This is fatal");
+		logger.trace("This is trace");
+		logger.debug("This is debug");
+		logger.info("This is info");
+		logger.warn("This is warn");
+		logger.error("This is error");
+		logger.fatal("This is fatal");
 		
 
 
